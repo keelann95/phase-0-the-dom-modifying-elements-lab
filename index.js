876f0118c421939ae827ef22b3b71bd40c09b297
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-04 19:15:25 +0300

    modified and added new files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+
+ const node=document.querySelector('main#main') 
+ const removedNode = node.remove('main#main')
+
+const newHeader = document.createElement('h1')
+ newHeader.id =('victory')
+ newHeader.textContent = 'Stephen is the champion'
